61259ce0c564b8cd6fefdb321515e7d44948223f
mdfaisalahmed025 <md.faisal3@northsouth.edu>
2023-08-07 12:54:06 +0600

    update dotenv file

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'welcome to quickticktes b2c d d';
+    return 'welcome';
   }
 }
