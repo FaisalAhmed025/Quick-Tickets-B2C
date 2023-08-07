1172ac0846119492140af1c424364e061a169bf3
mdfaisalahmed025 <md.faisal3@northsouth.edu>
2023-08-07 11:43:43 +0600

    update port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-require('dotenv')
 
 
-const PORT =  4000
+const PORT = process.env.PORT || 5000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
