78c71d986213718daf571dd2fa5d5f327e6fa07d
mdfaisalahmed025 <md.faisal3@northsouth.edu>
2023-08-08 12:28:55 +0600

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'welcome to Quick Tickets B2C';
+    return 'welcome to Quick Tickets';
   }
 }
